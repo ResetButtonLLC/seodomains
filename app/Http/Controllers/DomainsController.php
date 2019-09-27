36bb15d80bbcddb43fd018f04a8fe3c8db09b030
Aleksandr Kravchenko <a.kravchenko@promodo.com>
2019-09-27 11:11:36 +0300

    add excel export

--- app/Http/Controllers/DomainsController.php
+++ app/Http/Controllers/DomainsController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Domains;
 use Illuminate\Database\Eloquent\Builder;
-//use Rap2hpoutre\FastExcel\FastExcel;
+use Rap2hpoutre\FastExcel\FastExcel;
+use Illuminate\Support\Facades\DB;
 
 class DomainsController extends Controller {
 
@@ -17,7 +18,7 @@ class DomainsController extends Controller {
             foreach (array_keys($request->resource) as $key => $source) {
                 if ($key == 0) {
                     $domains = $domains->has($source);
-                }else{
+                } else {
                     $domains = $domains->orHas($source);
                 }
             }
@@ -40,9 +41,16 @@ class DomainsController extends Controller {
         }
 
         $domains = $domains->orderBy('url')->paginate(env('PAGE_COUNT'));
-//        if (isset($request->export)) {
-//            (new FastExcel($domains))->download('file.xlsx');
-//        }
+        if (isset($request->export)) {
+            $data = DB::table('domains')
+                    ->leftJoin('miralinks', 'domains.id', '=', 'miralinks.domain_id')
+                    ->leftJoin('rotapost', 'domains.id', '=', 'rotapost.domain_id')
+                    ->leftJoin('gogetlinks', 'domains.id', '=', 'gogetlinks.domain_id')
+                    ->leftJoin('sape', 'domains.id', '=', 'sape.domain_id')
+                    ->select('domains.url', 'miralinks.placement_price_rur as miralinks_price', 'miralinks.writing_price_rur as miralinks_writing_price', 'rotapost.placement_price  as rotapost_price', 'rotapost.writing_price  as rotapost_writing_price', 'gogetlinks.placement_price as gogetlinks_price', 'sape.placement_price as sape_price', 'miralinks.theme', 'miralinks.desc', 'miralinks.region', 'miralinks.google_index', 'miralinks.lang', 'miralinks.links', 'domains.ahrefs_dr', 'domains.ahrefs_inlinks', 'domains.ahrefs_outlinks', 'domains.serpstat_traffic')
+                    ->get();
+            return (new FastExcel($data))->download('file.xlsx');
+        }
         return view('domains.index', compact(['domains']));
     }
 
