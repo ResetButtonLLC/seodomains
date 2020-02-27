fe3c6df8e92512637ee14281d7114f7900dc4a1d
Alexey Shatrov <a.shatrov@promodo.com>
2020-02-27 16:49:14 +0200

    UPDATE: cookie ADD: update domains on Sat at 20-00

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('domains:batchupdate')->weeklyOn(6, '22:00');
     }
 
     /**
