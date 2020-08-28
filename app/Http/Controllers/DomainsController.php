c43cb54dca9564f710e47a15d8dfb07a68544c45
Aleksandr Kravchenko <a.kravchenko@promodo.com>
2020-08-28 12:12:24 +0300

    export fix

--- app/Http/Controllers/DomainsController.php
+++ app/Http/Controllers/DomainsController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\DB;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\Cell;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class DomainsController extends Controller {
 
@@ -48,11 +49,39 @@ class DomainsController extends Controller {
         if (isset($request->export)) {
             ini_set('max_execution_time', 0);
 
-            $domains = $domains->orderBy('domains.url', 'ASC')
-                    ->offset(35000)
-                    ->limit(38000)
+            $sites = $domains->orderBy('domains.url', 'ASC')
+                    ->offset(0)
+                    ->limit(35000)
                     ->get();
 
+            $data = $this->addData($request, $sites);
+
+            $sites = $domains->orderBy('domains.url', 'ASC')
+                    ->offset(35000)
+                    ->limit(35000)
+                    ->get();
+
+            $data = $this->addData($request, $sites, $data[0], $data[1], $data[2], $data[3]);
+//            dd($data);
+
+
+            return response()->download($data[0])->deleteFileAfterSend();
+
+            // Call writer methods here
+        } else {
+
+            $domains = $domains->orderBy('url')->paginate(env('PAGE_COUNT'));
+
+            foreach (Update::all() as $update_date) {
+                $update_dates[$update_date->name] = date('d-m-Y', strtotime($update_date->updated_at));
+            }
+
+            return view('domains.index', compact(['domains', 'update_dates']));
+        }
+    }
+
+    private function addData($request, $domains, $filename = null, $r = 0, $spreadsheet = null, $sheet = null) {
+        if (!$filename) {
             $spreadsheet = new Spreadsheet();
             $sheet = $spreadsheet->getActiveSheet();
 
@@ -83,103 +112,94 @@ class DomainsController extends Controller {
                     'A1'         // Top left coordinate of the worksheet range where
                     //    we want to set these values (default is A1)
             );
-
-
-            foreach ($domains as $r => $data) {
-                if (!$data->url)
-                    continue;
-                //Ряд Потому что эксель начинается с 1 а не с 0, первый ряд - заголовки
-                $row = $r + 2;
-                $column = 1;
-
-                //URL
-                $sheet->setCellValueByColumnAndRow($column, $row, $data->url);
-                $sheet->getCellByColumnAndRow($column, $row)->getHyperlink()->setUrl('http://' . $data->url);
-                $sheet->getStyleByColumnAndRow($column++, $row)->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLUE);
-
-                ///////Метрики
-                //Ahrefs
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->ahrefs_dr);
-
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->ahrefs_outlinks);
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->ahrefs_positions_top10);
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->ahrefs_traffic_top10);
-
-                //MIRALINKS (какой то баг с ссылкой - не работает прямая)
-                if (isset($request->resource['miralinks'])) {
-                    $sheet->setCellValueByColumnAndRow($column, $row, $data->miralinks['placement_price'] ? $data->miralinks['placement_price'] : '-');
-                    if ($data->miralinks['site_id']) {
-                        $sheet->getCellByColumnAndRow($column, $row)->getHyperlink()->setUrl('https://anonym.to/?"https://www.miralinks.ru/catalog/profileView/' . $data->miralinks['site_id'] . '"');
-                        $sheet->getStyleByColumnAndRow($column, $row)->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLUE);
-                    }
-                    $column++;
-
-                    $sheet->setCellValueByColumnAndRow($column, $row, $data->miralinks['writing_price'] ? $data->miralinks['writing_price'] : '-');
-                    if ($data->miralinks['site_id']) {
-                        $sheet->getCellByColumnAndRow($column, $row)->getHyperlink()->setUrl('https://anonym.to/?"https://www.miralinks.ru/catalog/profileView/' . $data->miralinks['site_id'] . '"');
-                        $sheet->getStyleByColumnAndRow($column, $row)->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLUE);
-                    }
-                    $column++;
-                }
-
-                //GOGETLINKS
-                if (isset($request->resource['gogetlinks'])) {
-                    $sheet->setCellValueByColumnAndRow($column++, $row, $data->gogetlinks['placement_price']);
-                }
-
-                //ROTAPOST
-                if (isset($request->resource['rotapost'])) {
-                    $sheet->setCellValueByColumnAndRow($column++, $row, $data->rotapost['placement_price']);
-                    $sheet->setCellValueByColumnAndRow($column++, $row, $data->rotapost['writing_price']);
-                }
-
-                //SAPE
-                if (isset($request->resource['sape'])) {
-                    $sheet->setCellValueByColumnAndRow($column++, $row, $data->sape['placement_price']);
-                }
-                //PRNEWS
-                if (isset($request->resource['prnews'])) {
-                    $sheet->setCellValueByColumnAndRow($column++, $row, $data->prnews['price']);
-                    $sheet->setCellValueByColumnAndRow($column++, $row, $data->prnews['audience']);
-                }
-                ////////////
-                //Тематика, регион, описание, язык
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->country);
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->miralinks ? $data->miralinks['theme'] : '');
-
-                //Google Index
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->miralinks ? $data->miralinks['google_index'] : '');
-
-                //Количество размещаемых ссылок (Миралинкс),
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->miralinks ? $data->miralinks['links'] : '');
-
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->ahrefs_inlinks);
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->miralinks ? $data->miralinks['lang'] : '');
-                //Majestic
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->majestic_cf);
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->majestic_tf);
-                $sheet->setCellValueByColumnAndRow($column++, $row, $data->miralinks ? $data->miralinks['desc'] : '');
-                //Serpstat
-                //$sheet->setCellValueByColumnAndRow($column++, $row, $data->serpstat_traffic);
-            }
-
-            $writer = new Xlsx($spreadsheet);
-            $filename = storage_path('app/domains-' . date('Y-m-d-H-i-s') . '.xlsx');
-            $writer->save($filename);
-
-            return response()->download($filename)->deleteFileAfterSend();
-
-            // Call writer methods here
-        } else {
-
-            $domains = $domains->orderBy('url')->paginate(env('PAGE_COUNT'));
-
-            foreach (Update::all() as $update_date) {
-                $update_dates[$update_date->name] = date('d-m-Y', strtotime($update_date->updated_at));
-            }
-
-            return view('domains.index', compact(['domains', 'update_dates']));
         }
+
+        foreach ($domains as $data) {
+            if (!$data->url)
+                continue;
+            //Ряд Потому что эксель начинается с 1 а не с 0, первый ряд - заголовки
+            $row = $r + 1;
+            $column = 1;
+            //URL
+            $sheet->setCellValueByColumnAndRow($column, $row, $data->url);
+            $sheet->getCellByColumnAndRow($column, $row)->getHyperlink()->setUrl('http://' . $data->url);
+            $sheet->getStyleByColumnAndRow($column++, $row)->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLUE);
+
+            ///////Метрики
+            //Ahrefs
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->ahrefs_dr);
+
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->ahrefs_outlinks);
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->ahrefs_positions_top10);
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->ahrefs_traffic_top10);
+
+            //MIRALINKS (какой то баг с ссылкой - не работает прямая)
+            if (isset($request->resource['miralinks'])) {
+                $sheet->setCellValueByColumnAndRow($column, $row, $data->miralinks['placement_price'] ? $data->miralinks['placement_price'] : '-');
+                if ($data->miralinks['site_id']) {
+                    $sheet->getCellByColumnAndRow($column, $row)->getHyperlink()->setUrl('https://anonym.to/?"https://www.miralinks.ru/catalog/profileView/' . $data->miralinks['site_id'] . '"');
+                    $sheet->getStyleByColumnAndRow($column, $row)->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLUE);
+                }
+                $column++;
+
+                $sheet->setCellValueByColumnAndRow($column, $row, $data->miralinks['writing_price'] ? $data->miralinks['writing_price'] : '-');
+                if ($data->miralinks['site_id']) {
+                    $sheet->getCellByColumnAndRow($column, $row)->getHyperlink()->setUrl('https://anonym.to/?"https://www.miralinks.ru/catalog/profileView/' . $data->miralinks['site_id'] . '"');
+                    $sheet->getStyleByColumnAndRow($column, $row)->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLUE);
+                }
+                $column++;
+            }
+
+            //GOGETLINKS
+            if (isset($request->resource['gogetlinks'])) {
+                $sheet->setCellValueByColumnAndRow($column++, $row, $data->gogetlinks['placement_price']);
+            }
+
+            //ROTAPOST
+            if (isset($request->resource['rotapost'])) {
+                $sheet->setCellValueByColumnAndRow($column++, $row, $data->rotapost['placement_price']);
+                $sheet->setCellValueByColumnAndRow($column++, $row, $data->rotapost['writing_price']);
+            }
+
+            //SAPE
+            if (isset($request->resource['sape'])) {
+                $sheet->setCellValueByColumnAndRow($column++, $row, $data->sape['placement_price']);
+            }
+            //PRNEWS
+            if (isset($request->resource['prnews'])) {
+                $sheet->setCellValueByColumnAndRow($column++, $row, $data->prnews['price']);
+                $sheet->setCellValueByColumnAndRow($column++, $row, $data->prnews['audience']);
+            }
+            ////////////
+            //Тематика, регион, описание, язык
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->country);
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->miralinks ? $data->miralinks['theme'] : '');
+
+            //Google Index
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->miralinks ? $data->miralinks['google_index'] : '');
+
+            //Количество размещаемых ссылок (Миралинкс),
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->miralinks ? $data->miralinks['links'] : '');
+
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->ahrefs_inlinks);
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->miralinks ? $data->miralinks['lang'] : '');
+//            dd($column++, $row);
+            //Majestic
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->majestic_cf);
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->majestic_tf);
+            $sheet->setCellValueByColumnAndRow($column++, $row, $data->miralinks ? $data->miralinks['desc'] : '');
+            //Serpstat
+            //$sheet->setCellValueByColumnAndRow($column++, $row, $data->serpstat_traffic);
+            $r++;
+        }
+
+        $writer = new Xlsx($spreadsheet);
+        if (!$filename) {
+            $filename = storage_path('app/domains-' . date('Y-m-d-H-i-s') . '.xlsx');
+        }
+        $writer->save($filename);
+
+        return [$filename, $row, $spreadsheet, $sheet];
     }
 
 }
