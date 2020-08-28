1905308f45f32855fbd13d2a215e9ba1aa3aa0d3
Aleksandr Kravchenko <a.kravchenko@promodo.com>
2020-08-28 09:51:46 +0300

    export fix

--- app/Http/Controllers/DomainsController.php
+++ app/Http/Controllers/DomainsController.php
@@ -44,7 +44,7 @@ class DomainsController extends Controller {
         if ($request->price_to) {
             $domains = $domains->where('placement_price', '<=', $request->price_to);
         }
-        
+
         if (isset($request->export)) {
             ini_set('max_execution_time', 0);
 
@@ -85,7 +85,7 @@ class DomainsController extends Controller {
 
 
             foreach ($domains as $r => $data) {
-                if(!$data->url)
+                if (!$data->url)
                     continue;
                 //Ряд Потому что эксель начинается с 1 а не с 0, первый ряд - заголовки
                 $row = $r + 2;
@@ -107,17 +107,17 @@ class DomainsController extends Controller {
                 //MIRALINKS (какой то баг с ссылкой - не работает прямая)
                 if (isset($request->resource['miralinks'])) {
                     $sheet->setCellValueByColumnAndRow($column, $row, $data->miralinks['placement_price'] ? $data->miralinks['placement_price'] : '-');
-//                    if ($data->miralinks['placement_price'] && $data->miralinks['site_id']) {
-                        $sheet->getCellByColumnAndRow($column, $row)->getHyperlink()->setUrl('https://www.miralinks.ru/catalog/profileView/' . $data->miralinks['site_id']);
-                        $sheet->getStyleByColumnAndRow($column, $row)->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLUE);
-//                    }
+                    if ($data->miralinks['site_id']) {
+                        $sheet->getCellByColumnAndRow($column, $row)->getHyperlink()->setUrl('https://anonym.to/?https://www.miralinks.ru/catalog/profileView/' . $data->miralinks['site_id']);
+                    }
+                    $sheet->getStyleByColumnAndRow($column, $row)->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLUE);
                     $column++;
 
                     $sheet->setCellValueByColumnAndRow($column, $row, $data->miralinks['writing_price'] ? $data->miralinks['writing_price'] : '-');
-//                    if ($data->miralinks['writing_price'] && $data->miralinks['site_id']) {
-                        $sheet->getCellByColumnAndRow($column, $row)->getHyperlink()->setUrl('https://www.miralinks.ru/catalog/profileView/' . $data->miralinks['site_id']);
-                        $sheet->getStyleByColumnAndRow($column, $row)->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLUE);
-//                    }
+                    if ($data->miralinks['site_id']) {
+                        $sheet->getCellByColumnAndRow($column, $row)->getHyperlink()->setUrl('https://anonym.to/?https://www.miralinks.ru/catalog/profileView/' . $data->miralinks['site_id']);
+                    }
+                    $sheet->getStyleByColumnAndRow($column, $row)->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLUE);
                     $column++;
                 }
 
