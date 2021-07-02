580794aee1ab9f47e2f406d1b21b64b8e92e1b93
dennikolenko <d.nikolenko@promodo.com>
2021-07-02 11:57:30 +0300

    fix miralink

--- app/Console/Commands/MiralinksCommand.php
+++ app/Console/Commands/MiralinksCommand.php
@@ -189,7 +189,7 @@ class MiralinksCommand extends ParserCommand {
             "Postman-Token: 35270faf-569e-4a30-a508-1dfb926ccc94,5a3d62cb-fc51-4915-96cf-452f2b0569f7",
             "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
             "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-            "accept-encoding: gzip, deflate, br",
+            //"accept-encoding: gzip, deflate, br",
             "accept-language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6",
             "cache-control: no-cache,no-cache",
             "referer: https://www.miralinks.ru/users/login,https://www.miralinks.ru/",
