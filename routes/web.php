74c4b8a816881ebfe5950ea6588af49aaf374fed
dennikolenko <d.nikolenko@promodo.com>
2021-08-31 18:46:56 +0300

    add azure auth

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 Route::get('/', 'CommonController@main')->name('main'); //Страница с формой логина
-Route::get('/login', '\App\Http\Middleware\AppAzure@azure')->name('login'); //Обработчик логина через азур
-Route::get('/logout', '\App\Http\Middleware\AppAzure@logout')->name('logout');
-Route::get('/login/azurecallback', '\App\Http\Middleware\AppAzure@azurecallback');
 
-Route::group(['middleware' => ['azure']], function () {
+Route::get('/login', [\Promodo\LaravelAzureAuth\Azure::class, 'azure'])->name('login');
+Route::get('/logout', [\Promodo\LaravelAzureAuth\Azure::class, 'azurelogout'])->name('logout');
+Route::get('/login/azurecallback', [\Promodo\LaravelAzureAuth\Azure::class, 'azurecallback']);
+
+Route::group(['middleware' => ['auth']], function () {
     Route::get('/domains', 'DomainsController@index')->name('domains');
     Route::get('/dr-price', 'DomainsController@averagePriceForDr')->name('dr-price');
     Route::get('/cookies', 'CookieController@show')->name('cookies.show');
