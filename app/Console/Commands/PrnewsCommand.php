bc719e210028cf304ff1304a20ac0292028945ec
x1ds <x1dsnikos@gmail.com>
2022-01-20 18:09:58 +0200

    fix and update

--- app/Console/Commands/PrnewsCommand.php
+++ app/Console/Commands/PrnewsCommand.php
@@ -2,11 +2,11 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
 use App\Models\{
     Domains,
     Prnews
 };
+use Illuminate\Support\Facades\Artisan;
 use Symfony\Component\DomCrawler\Crawler;
 use App\Helpers\DomainsHelper;
 
@@ -24,19 +24,15 @@ class PrnewsCommand extends ParserCommand {
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Get domains from prnews';
 
     /**
      * Create a new command instance.
      *
      * @return void
      */
-
-    private $logfolder;
-
     public function __construct() {
         parent::__construct();
-        $this->logfolder = storage_path('parserlogs/prnews/');
     }
 
     /**
@@ -47,191 +43,16 @@ class PrnewsCommand extends ParserCommand {
     public function handle() {
         $this->initLog('prnews');
 
-        $this->writeLog('prnews.io');
-        $page = 1;
+        $this->writeLog('prnews get with dusk test');
 
-        $counter = array(
-            'current' => 0,
-            'total' => 0,
-            'new' => 0,
-            'updated' => 0,
-        );
-
-        //Курс гривны к рублю
-        $exchange_rates = json_decode(file_get_contents('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11'));
-        foreach ($exchange_rates as $exchange_rate) {
-            if ($exchange_rate->ccy == "RUR") {
-                $uah_to_rur = $exchange_rate->buy;
-            }
-        }
-
-        //Получаем все домены
-        $db_domains = Domains::all('id','url');
-
-        while ($counter['current'] <= $counter['total']) {
-
-            $data = $this->getData($page);
-
-            if (!$this->checkLoggedIn($data)) {
-                die();
-            }
-
-            if ($page == 1) {
-                //Количество сайтов на площадке
-                $dom = new Crawler($data);
-                $counter['total'] =  $dom->filter('div.cards-meta_counter')->text();
-                $counter['total'] = preg_replace('/[^0-9]/', '',  $counter['total']);
-            }
-
-
-            $dom = new Crawler($data);
-            $sites = $dom->filter('div.js__data-platform-click')->each(function ($content, $i) {
-                return  $content->html();
-            });
-
-            foreach ($sites as $site) {
-
-                $row_dom = new Crawler($site);
-
-                //file_put_contents($log_folder.'/card.html',$row_dom->html());
-                $data = [];
-
-                $data['url'] = utf8_encode($row_dom->filter('div.card_url')->first()->text());
-                $data['url'] = preg_replace('/^www\./','',$data['url']);
-
-                $data['price'] = $row_dom->filter('div.card_price')->first()->text();
-                $data['price'] = preg_replace('/[^0-9,]/','',$data['price']);
-                $data['price'] = str_ireplace(',','.',$data['price']);
-                $data['price'] = (int)round($data['price']/$uah_to_rur,0);
-
-                $data['audience'] = trim($row_dom->filter('div.card_audience')->first()->text());
-
-                if ($data) {
-
-                    $data['domain_id'] = DomainsHelper::getIdByUrl($db_domains,$data['url']);
-                    if (!$data['domain_id']) {
-                        $data['domain_id'] = Domains::insertGetId(['url' => $data['url'], 'created_at' => date('Y-m-d H:i:s')]);
-                    }
-
-                    $prnews = Prnews::updateOrCreate(
-                        ['domain_id' => $data['domain_id']],
-                        ['price' => $data['price'], 'audience' => $data['audience']]
-                    );
-
-                    if($prnews->created_at == $prnews->updated_at) {
-                        $counter['new']++;
-                    } else {
-                        $counter['updated']++;
-                    }
-
-                    $counter['current']++;
-
-                }
-
-                //dd($data);
-            }
-            $antiban_pause = mt_rand(0, 0);
-            $this->writeLog('prnews.io page : ' . $page . ' | Fetched domains : ' . count($sites) . ' | Progress '.$counter['current'].'/'.$counter['total'].' | Added total : ' . $counter['new'] . ' | Updated total : ' . $counter['updated'].' | Sleeping for ' . $antiban_pause . ' seconds');
-            sleep($antiban_pause);
-
-            $page++;
-        }
+        Artisan::call('dusk', [
+            ' --filter' => 'PrnewsTest',
+        ]);
 
         $this->call('domains:finalize', [
             '--table' => 'prnews',
-            '--hours' => 3
         ]);
-        //dd($this->getData());
 
+        return true;
     }
-
-    private function getData($page)
-    {
-        $url = $page == 1 ? 'https://prnews.io/ru/sites/perpage/96/' : 'https://prnews.io/ru/sites/page/'. $page .'/perpage/96/';
-
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_COOKIEJAR, tempnam(sys_get_temp_dir(), 'PRNEWS_COOKIE_FILE'));
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->getCookie());
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Connection: keep-alive",
-            "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-            "accept-language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6",
-            "cache-control: no-cache,no-cache",
-        ));
-
-        $page_valid = false;
-
-        while (!$page_valid) {
-            $curl_response = curl_exec($ch);
-
-            $page_valid = stripos($curl_response,'<div class="cards-meta">');
-            if (!$page_valid) {
-                $antiban_pause = mt_rand(30, 50);
-                $this->writeLog('Prnews.ru | Get empty responce | sleeping for '.$antiban_pause.' seconds');
-                sleep($antiban_pause);
-            }
-        }
-
-        file_put_contents($this->logfolder.'/'.$page.'.html',$curl_response);
-
-        curl_close($ch);
-
-        return $curl_response;
-    }
-
-    private function isLogged() : string
-    {
-        $ch = curl_init('https://prnews.io/account/');
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_COOKIEJAR, tempnam(sys_get_temp_dir(), 'PRNEWS_COOKIE_FILE'));
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->getCookie());
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Connection: keep-alive",
-            "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-            "accept-language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6",
-            "cache-control: no-cache,no-cache",
-        ));
-
-        $html = curl_exec($ch);
-
-        if (!strpos($html, 'https://prnews.io/api/logout')) {
-            $this->writeLog('Prnews : Login not successful : Most likely that the cookies has expired');
-            file_put_contents($this->logfolder.'/login.html',$html);
-            return false;
-        } else {
-            $this->writeLog('Auth successfull');
-            return $html;
-        }
-
-    }
-
-    private function checkLoggedIn($page_content) : bool
-    {
-        if (strpos($page_content, 'link-signup')) {
-            $this->writeLog('Prnews : Login not successful : Most likely that the cookies has expired');
-            file_put_contents($this->logfolder.'/bad_page.html',$page_content);
-            $this->sendErrorNotification('auth error most likely that the cookies has expired');
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-
-
 }
